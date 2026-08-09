631f92bfca3d605d958ce5e276865a7b39502455
dharshinis02122003 <dharshinis02122003@gmail.com>
2026-08-09 12:35:17 +0530

    Update hello.py

--- hello.py
+++ hello.py
@@ -1,2 +1,13 @@
 #program 1
 print("Hello from Jenkins!")
+pipeline {
+    agent any
+
+    stages {
+        stage('Run Python Script') {
+            steps {
+                bat 'python hello.py'
+            }
+        }
+    }
+}
